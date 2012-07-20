759795a2f157fa738bee582dc1e36f3ae3ffdfd8
Swizec Teller <swizec@swizec.com>
2012-07-20 18:38:02 +0200

    mutate can do one of three things

--- code/Operators/Basic.hs
+++ code/Operators/Basic.hs
@@ -5,11 +5,25 @@ module Operators.Basic (
 
 import System.Random
 
-mutate::(RandomGen g) => g -> [Char] -> (Bool, g)
-mutate gen s = should gen
+mutate::(RandomGen g) => g -> [Char] -> ([Char], g)
+mutate gen s
+  | typ == 2 = (change place s, gen')
+  | typ == 1 = (remove place s, gen')
+  | typ == 0 = (add place s, gen')
+  where (typ, place, gen') = choose gen $ length s
 
-should::(RandomGen g) => g -> (Bool, g)
-should gen =
-  let (i, g) = next gen
-  in if fromIntegral (i `mod` 10) / 10 > 0.2 then (False, g)
-     else (True, g)
+-- chooses the type of mutation and the character where it happens
+choose::(RandomGen g) => g -> Int -> (Int, Int, g)
+choose gen len =
+  let (typ, g) = randomR (0, 2) gen
+      (place, g') = randomR (0, len-1) gen
+  in (typ, place, g')
+
+change::Int -> [Char] -> [Char]
+change p s = "change"
+
+remove::Int -> [Char] -> [Char]
+remove p s = "remove"
+
+add::Int -> [Char] -> [Char]
+add p s = "add"
