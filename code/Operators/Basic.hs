1add861fa8e35b5f0c52696498f5bafc0878cf49
Swizec Teller <swizec@swizec.com>
2012-07-20 23:46:14 +0200

    basic mutation operators implemented

--- code/Operators/Basic.hs
+++ code/Operators/Basic.hs
@@ -4,12 +4,13 @@ module Operators.Basic (
   ) where
 
 import System.Random
+import Data.List
 
 mutate::(RandomGen g) => [Char] -> g -> ([Char], g)
 mutate s gen
-  | typ == 2 = (change place s, gen')
+  | typ == 2 = (change place s gen', gen')
   | typ == 1 = (remove place s, gen')
-  | typ == 0 = (add place s, gen')
+  | typ == 0 = (add place s gen', gen')
   where (typ, place, gen') = choose (length s) gen
 
 -- chooses the type of mutation and the character where it happens
@@ -19,11 +20,21 @@ choose len gen =
       (place, g') = randomR (0, len-1) gen
   in (typ, place, g')
 
-change::Int -> [Char] -> [Char]
-change p s = "change"
+change::(RandomGen g) => Int -> [Char] -> g -> [Char]
+change p s gen =
+  let chars = " abcdefghijklmnopqrtstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ"
+      (c, gen') = randomR (0, (-1 +)$length chars) gen
+      (left, right) = splitAt p s
+  in left ++ chars!!c:right
 
 remove::Int -> [Char] -> [Char]
-remove p s = "remove"
+remove p s =
+  let (left, right) = splitAt p s
+  in left ++ tail right
 
-add::Int -> [Char] -> [Char]
-add p s = "add"
+add::(RandomGen g) => Int -> [Char] -> g -> [Char]
+add p s gen =
+  let chars = " abcdefghijklmnopqrtstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ"
+      (c, gen') = randomR (0, (-1 +)$length chars) gen
+      (left, right) = splitAt p s
+  in left ++ chars!!c:tail right
