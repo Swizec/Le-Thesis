bedf0349c3526f92cf9e687375552fb370266897
Swizec Teller <swizec@swizec.com>
2012-07-20 23:56:45 +0200

    cleaned up mutator code

--- code/Operators/Basic.hs
+++ code/Operators/Basic.hs
@@ -8,9 +8,9 @@ import Data.List
 
 mutate::(RandomGen g) => [Char] -> g -> ([Char], g)
 mutate s gen
-  | typ == 2 = (change place s gen', gen')
+  | typ == 2 = change place s gen'
   | typ == 1 = (remove place s, gen')
-  | typ == 0 = (add place s gen', gen')
+  | typ == 0 = add place s gen'
   where (typ, place, gen') = choose (length s) gen
 
 -- chooses the type of mutation and the character where it happens
@@ -20,21 +20,25 @@ choose len gen =
       (place, g') = randomR (0, len-1) gen
   in (typ, place, g')
 
-change::(RandomGen g) => Int -> [Char] -> g -> [Char]
+change::(RandomGen g) => Int -> [Char] -> g -> ([Char], g)
 change p s gen =
-  let chars = " abcdefghijklmnopqrtstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ"
-      (c, gen') = randomR (0, (-1 +)$length chars) gen
+  let (c, gen') = randChar gen
       (left, right) = splitAt p s
-  in left ++ chars!!c:right
+  in (left ++ c:right, gen')
 
 remove::Int -> [Char] -> [Char]
 remove p s =
   let (left, right) = splitAt p s
   in left ++ tail right
 
-add::(RandomGen g) => Int -> [Char] -> g -> [Char]
+add::(RandomGen g) => Int -> [Char] -> g -> ([Char], g)
 add p s gen =
+  let (c, gen') = randChar gen
+      (left, right) = splitAt p s
+  in (left ++ c:tail right, gen')
+
+randChar::(RandomGen g) => g -> (Char, g)
+randChar gen =
   let chars = " abcdefghijklmnopqrtstuvwxyzABCDEFGHIJKLMNOPQRTSUVWXYZ"
       (c, gen') = randomR (0, (-1 +)$length chars) gen
-      (left, right) = splitAt p s
-  in left ++ chars!!c:tail right
+  in (chars!!c, gen')
